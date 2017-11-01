f4aea9bc50018b1e74b960736816a577694b4ee6
Joseph Kwon <kwonjosepho@gmail.com>
2017-11-01 15:26:59 -0700

    (bug) fix bug to allow correct port on heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/../dist/'));
-
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+const port = process.env.PORT || 1337
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
 });
